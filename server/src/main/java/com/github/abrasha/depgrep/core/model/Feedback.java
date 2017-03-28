6b996c3fea7b9d2ccaa58f3587cb38ebf898e8ef
Andrii <aabrasha@gmail.com>
2017-03-28 19:09:43 +0300

    - fix integration tests

--- server/src/main/java/com/github/abrasha/depgrep/core/model/Feedback.java
+++ server/src/main/java/com/github/abrasha/depgrep/core/model/Feedback.java
@@ -3,10 +3,8 @@ package com.github.abrasha.depgrep.core.model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import javax.validation.constraints.Min;
 
 /**
  * @author Andrii Abramov on 3/11/17.
@@ -17,10 +15,7 @@ import javax.validation.constraints.Min;
 @Table(name = "feedbacks")
 public class Feedback extends BaseEntity {
     
-    @Column(unique = true)
     private String artifactId;
-    
-    @Min(0)
     private Integer timesApproved;
     
 }
